77e6bce3dbd6b74cc85e5bb637cd5e6f488024cc
gjGameJam <grantjbenson@icloud.com>
2025-01-01 19:11:21 -0500

    started ui work, changed background and started distance graphing

---
+++ FoldedCSC/Assets/Scripts/UI/DistanceGraphing.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class DistanceGraphing : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //referenced: https://www.youtube.com/watch?v=5ZBynjAsfwI to learn about line renderers
+    // I plan to have this graph as a collection of points (being the furthest distance in each round)
+    // and connect each point at the end of each round once the data is available
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AddDataPoint(float distance)
+    {
+        //add data point to graph
+
+        //and create line from previous data point to connect them
+    }
+}
